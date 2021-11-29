5277f26320e2dd66f6124b16b2655272914e564d
cheher21 <cheher@my365.bellevue.edu>
2021-11-28 22:22:16 -0600

    Module 7

---
+++ module_7/Python_Connector_Test.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports user",
+    "password": "fender09",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
